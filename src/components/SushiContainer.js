7c35c79262668d451bd76252e9c79bab1b0d7ef1
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-06-20 21:17:47 -0400

    Sushi on page

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,10 +1,23 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ sushiArray }) {
+  // console.log(sushiArray)
+  const displayedSushi = sushiArray.map(sushi => {
+    return (
+      <Sushi 
+        key={sushi.id}
+        name={sushi.name}
+        image={sushi.img_url}
+        created={sushi.create_at}
+        price={sushi.price}
+      />
+    )
+  })
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
+      {displayedSushi}
       <MoreButton />
     </div>
   );
